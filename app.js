afca0663ec1fe3af5fad1b05c3b4adbff7e33911
Laurapederiva <laura.pederiva@mi.unc.edu.ar>
2023-01-18 16:23:48 -0300

    actualizacion de datos para render

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
 app.use( express.static(publicPath) );
 
-app.listen(3001, () =>{
-    console.log("Servidor corriendo exitosamente en puerto 3001")
+const port = process.env.PORT || 3001;
+app.listen(port, () =>{ console.log(`Servidor corriendo exitosamente en puerto ${port}`)
 });
 
 app.get("/", (req, res) => {
