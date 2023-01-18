f2323f76b4ac1dc588b0462b4e82b90f7fdde4f9
Laurapederiva <laura.pederiva@mi.unc.edu.ar>
2023-01-18 15:36:21 -0300

    subo proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express ();
+
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath) );
+
+app.listen(3001, () =>{
+    console.log("Servidor corriendo exitosamente en puerto 3001")
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
